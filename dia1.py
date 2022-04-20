c5ebcf25b763a2dc3ad2d4a406a9cdfaeb566e29
tintayadev <pau.tintaya.code@gmail.com>
2022-04-20 09:04:33 -0400

    Day 1

---
+++ dia1.py
@@ -0,0 +1,9 @@
+"""
+                Biienvenidos al dia 1 de #100diasdepython
+                    El reto de hoy es:
+        Define una variable numerica e imprime el valor de tu variable
+
+"""
+
+number = 999
+print("El valor de tu variable es: ", number)
