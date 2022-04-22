6bfbe62ee1287f6b04ffcbd4d485c0656b20a79e
tintayadev <pau.tintaya.code@gmail.com>
2022-04-22 09:12:37 -0400

    Day 2 and  3

---
+++ archivo.py
@@ -0,0 +1,8 @@
+"""
+                Biienvenidos al dia 3 de #100diasdepython
+                    El reto de hoy es:
+        Declara una variable de tipo cadena que tenga un texto divertido e imprimela
+
+"""
+cadena = "Mensaje divertido cargando..."
+print(cadena)
---
+++ dia2.py
@@ -0,0 +1,8 @@
+"""
+                Biienvenidos al dia 2 de #100diasdepython
+                    El reto de hoy es:
+            Resta de 2 numeros enteros e imprime el resultado
+
+"""
+resta = 99 - 8
+print(resta) 
