6fb5595fd0090e0bc50c59c97ec8a8228585b6e8
tintayadev <pau.tintaya.code@gmail.com>
2022-04-25 09:43:18 -0400

    Dia 6

---
+++ dia4.py
@@ -0,0 +1,8 @@
+"""
+            Biienvenidos al dia 4 de #100diasdepython
+                El reto de hoy es:
+    Usa la funcion type()para imprimir el tipo de una variable booleana
+
+"""
+var_bool = False
+print(type(var_bool))
---
+++ dia5.py
@@ -0,0 +1,8 @@
+"""
+            Biienvenidos al dia 5 de #100diasdepython
+                El reto de hoy es:
+    Declara una lista de numero y encunetra el valor mas pequeño
+
+"""
+lista_de_numeros = [43, 5, 12, 53, 12, 1, -12, 9999]
+print(min(lista_de_numeros))
