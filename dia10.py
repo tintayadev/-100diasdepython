f0c44428643d540e8e29b651985960419554f923
tintayadev <pau.tintaya.code@gmail.com>
2022-04-29 23:27:47 -0400

    Dia 10

---
+++ dia10.py
@@ -0,0 +1,8 @@
+"""
+            Biienvenidos al dia 10 de #100diasdepython
+                El reto de hoy es:
+    Inicializa 3 variables numericas de forma multiple e imprime 
+        sus valores en una sola linea
+"""
+n1, n2, n3 = 1, 2, 3
+print(n1, n2, n3)
---
+++ dia8.py
@@ -0,0 +1,7 @@
+"""
+            Biienvenidos al dia 8 de #100diasdepython
+                El reto de hoy es:
+    Declara una lista de de numeros e imprime el numero de elementos
+"""
+lista_nums = [1,2,3,4,5]
+print(len(lista_nums))
