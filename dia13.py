20beeca1f7d8cf485d0484040799a38e937d6fd4
tintayadev <pau.tintaya.code@gmail.com>
2022-05-02 20:03:01 -0400

    Dia 13

---
+++ dia13.py
@@ -0,0 +1,9 @@
+"""
+    Biienvenidos al dia 13 de #100diasdepython
+            El reto de hoy es:
+Decalara dos variables booleanas, evalua una expresion logica
+    usando dos operadores logicos e imprime el resultado
+"""
+bool_1 = True
+bool_2 = False
+print( not bool_1 and bool_2)
