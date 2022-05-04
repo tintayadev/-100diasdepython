05ed75761f7689634e8fe3c4096ed7aafa9fcf60
tintayadev <pau.tintaya.code@gmail.com>
2022-05-03 23:48:11 -0400

    Dia 14

---
+++ dia14.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 14 de #100diasdepython
+            El reto de hoy es:
+Declara una variable mediante una expresion aritmetica
+usando dos operadores aritmeticos e imprime el resultado
+"""
+variable = 1 + 2 ** 2
+print(variable)
