acb7650420767d8b65082dbee3902e27e24e94e0
tintayadev <pau.tintaya.code@gmail.com>
2022-05-04 23:36:35 -0400

    Day 15

---
+++ dia15.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 15 de #100diasdepython
+            El reto de hoy es:
+Declara una variable de tipo cadena e imprime los ultimos tres
+caracters de la cadena sin usar ciclos
+"""
+variable = "Soy una cadena Uwu"
+print(variable[-3:])
