caddb7272867de3e3349dada50e8a9f8ec3e95fc
tintayadev <pau.tintaya.code@gmail.com>
2022-05-06 00:40:05 -0400

    Day 17

---
+++ dia16.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 16 de #100diasdepython
+            El reto de hoy es:
+declara una variable de tipo cadena e imprime la cantidad
+de caracteres de la cadena sin usar ciclos
+"""
+cadena = "Soy una cadena Wiii!!"
+print(len(cadena))
---
+++ dia17.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 17 de #100diasdepython
+            El reto de hoy es:
+Declara una variable de tipo cadena, encuentra el numero de veces 
+que aparece la letra 'a' in usar ciclos e imprime el resultados
+"""
+cadena = "Soy una cadena Wiii!!"
+print(cadena.count('a'))
