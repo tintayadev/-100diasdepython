e45bd40bdb0503e555ab8e56505c03fed1228626
tintayadev <pau.tintaya.code@gmail.com>
2022-05-09 14:28:08 -0400

    Day 18

--- dia18.py
+++ dia18.py
@@ -4,9 +4,6 @@
 Declara una variable de tipo cadena, encuentra el primer
 y el ultimo caracter en orden lexografico sin usar ciclos e imprimelos
 """
-from gettext import find
-from sys import maxunicode
-
 cadena = "python"
 primero = min(cadena)
 ultimo = max(cadena)
