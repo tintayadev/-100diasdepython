6f1c34a334c33d4f4f34cdb858159097fb49dbc1
tintayadev <pau.tintaya.code@gmail.com>
2022-05-09 08:42:41 -0400

    Day 19

---
+++ dia19.py
@@ -0,0 +1,10 @@
+"""
+    Biienvenidos al dia 19 de #100diasdepython
+            El reto de hoy es:
+Declara una variable de tipo cadena, reviertala sin usar
+funciones adicioonales e imprime el resultado
+"""
+cadena = "cadena normal"
+reverso = cadena[::-1]
+print(reverso)
+
