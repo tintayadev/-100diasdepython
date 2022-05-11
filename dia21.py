b4b5bb3f007049fd4f4fcceb2a81a9578be6c9c0
tintayadev <pau.tintaya.code@gmail.com>
2022-05-11 00:08:16 -0400

    Day 21

---
+++ dia21.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 21 de #100diasdepython
+            El reto de hoy es:
+Utiliza lo aprendido en el anterior reto para 
+encontrar el mensaje escondido: 'hjfacetiluzislcafiesdolavedfiedesno'
+"""
+cadena = 'hjfacetiluzislcafiesdolavedfiedesno'
+print(cadena[2::3])
