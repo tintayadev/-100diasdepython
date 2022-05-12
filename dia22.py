dbb6e373a2de89b876412b0af446b20632f6ecbf
tintayadev <pau.tintaya.code@gmail.com>
2022-05-11 23:53:24 -0400

    Day 22

---
+++ dia22.py
@@ -0,0 +1,9 @@
+"""
+    Biienvenidos al dia 22 de #100diasdepython
+            El reto de hoy es:
+Utiliza funciones de cadenas para crear una lista
+de palabras con la siguiente cadena: 'Los pequeños pasos te llevan a algo más grande'
+"""
+cadena = 'Los pequeños pasos te llevan a algo más grande'
+lista = cadena.split(" ")
+print(lista)
