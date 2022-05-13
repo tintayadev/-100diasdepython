07373c1a1fdfbc59174f246b7c88f2ced7e80e54
tintayadev <pau.tintaya.code@gmail.com>
2022-05-12 23:52:12 -0400

    Dia 23

---
+++ dia23.py
@@ -0,0 +1,9 @@
+"""
+    Biienvenidos al dia 23 de #100diasdepython
+            El reto de hoy es:
+Utiliza funciones de cadenas para quitar los espacios inncesarios de la siguiente cadena:
+cadena = "       Python es divertido       "
+"""
+cadena = "       Python es divertido       "
+cadena = cadena.strip()
+print(cadena)
