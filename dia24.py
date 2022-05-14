5ce2c74529c8cee796a5082d7fe1657c69549cb7
tintayadev <pau.tintaya.code@gmail.com>
2022-05-13 23:07:30 -0400

    Day 24

---
+++ dia24.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 24 de #100diasdepython
+            El reto de hoy es:
+Crea una lista de compras e imprime del 2do
+al 4to elemento
+"""
+lista_compras = ["10 Panes", "1/2 doc. de Huevos", "Leche de soja", "1 doc. de Nucitas", "Pan integral"]
+print(lista_compras[1:4])
