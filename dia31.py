f7266b1cd3cdf3908b3d1479ec93657a51124c57
tintayadev <pau.tintaya.code@gmail.com>
2022-05-21 11:55:56 -0400

    Day 31

---
+++ dia31.py
@@ -0,0 +1,11 @@
+"""
+    Biienvenidos al dia 31 de #100diasdepython
+            El reto de hoy es:
+Declara dos valores booleanos, convierte una a entero y la otra en cadena
+imprime el resultado en una sola linea
+"""
+var_a = True
+var_b = False
+entero = int(var_a)
+cadena = str(var_b)
+print(entero, cadena)
