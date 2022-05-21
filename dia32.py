bf78ec9d8fed695efc6bb05baf8833d2cede2169
tintayadev <pau.tintaya.code@gmail.com>
2022-05-21 11:56:09 -0400

    Day32

---
+++ dia32.py
@@ -0,0 +1,11 @@
+"""
+    Biienvenidos al dia 32 de #100diasdepython
+            El reto de hoy es:
+Declara dos listas, una vacia y otra con elementos convierte
+ambas listas en booleanos, imprime el resultados en una sola linea
+"""
+lista_a = []
+lista_b = [1,0,0,1,1]
+bool_a = bool(lista_a)
+bool_b = bool(lista_b)
+print(bool_a, bool_b)
