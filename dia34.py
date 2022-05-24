0bf09fe4cd4fc349ead608e6252010959e618b96
tintayadev <pau.tintaya.code@gmail.com>
2022-05-24 10:00:26 -0400

    Day 34

---
+++ dia34.py
@@ -0,0 +1,14 @@
+"""
+    Biienvenidos al dia 34 de #100diasdepython
+            El reto de hoy es:
+Declara un diccionario de 5 palabras relacionadas 
+a la programacion con su definicion. Imprime el resultado
+"""
+dic = {
+    "Python" : "Lenguaje de alto nivel de programacion interpretado",
+    "Javascript" : "Lenguaje de programacion interpretado que usa el standar ECMAScript",
+    "Java" : "Java es un lenguaje de programación orientado a objetos que permite el desarrollo de aplicaciones de manera fácil y sencilla" ,
+    "Kotlin" : "Kotlin es un lenguaje de programación de tipado estático que corre sobre la máquina virtual de Java y que también puede ser compilado a código fuente de JavaScript.",
+    "Dart" : "Dart es un lenguaje de programación de código abierto, desarrollado por Google."
+}
+print(dic)
