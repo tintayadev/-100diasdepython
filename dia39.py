e7196a23958551b43f9b7142cf6d5aa8382568cc
tintayadev <pau.tintaya.code@gmail.com>
2022-05-28 17:50:28 -0400

    Day 39

---
+++ dia39.py
@@ -0,0 +1,9 @@
+"""
+    Biienvenidos al dia 38 de #100diasdepython
+            El reto de hoy es:
+Utiliza el conjunto del reto anterior y verifica si tienes
+un gato en el conjunto. Imprime el resultado.
+"""
+animales = {"Pinguino", "Flamenco", "Panda", "Leon", "Serpiente"}
+existe_gato = "Gato" in animales
+print(existe_gato)
