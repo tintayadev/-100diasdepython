150c0e0c7cb2bb71cb7288e27e22d3592924a8cf
tintayadev <pau.tintaya.code@gmail.com>
2022-05-28 17:50:07 -0400

    Day 38

---
+++ dia38.py
@@ -0,0 +1,7 @@
+"""
+    Biienvenidos al dia 38 de #100diasdepython
+            El reto de hoy es:
+Declara un conjunto de 5 animales e imprime el resultado
+"""
+animales = {"Pinguino", "Flamenco", "Panda", "Leon", "Serpiente"}
+print(animales)
