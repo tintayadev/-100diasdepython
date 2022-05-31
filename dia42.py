187a48d8d79f9df3fdeb2df35f95e60a2b62de3e
tintayadev <pau.tintaya.code@gmail.com>
2022-05-31 10:35:02 -0400

    Day 42

---
+++ dia42.py
@@ -0,0 +1,9 @@
+"""
+    Biienvenidos al dia 42 de #100diasdepython
+            El reto de hoy es:
+Utiliza los conjuntos del reto anterior para encontrar la union
+de ambos conjuntos sin usar ciclos e imprime el resultado
+"""
+animales = {"Pinguino", "Flamenco", "Panda", "Leon", "Serpiente"}
+otros_animales = {"Gato", "Perros", "Panda rojo", "Leon", "Flamenco"}
+print(animales.union(otros_animales))
