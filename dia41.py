95638a262d117f05b43efb7b8b9af337ebef63d8
tintayadev <pau.tintaya.code@gmail.com>
2022-05-30 23:20:02 -0400

    Day 41

---
+++ dia41.py
@@ -0,0 +1,9 @@
+"""
+    Biienvenidos al dia 41 de #100diasdepython
+            El reto de hoy es:
+Utiliza el conjunto del reto anterior, define un nuevo conjunto de mascotas,
+encuentra su interseccion de ambos conjuntos sin usar ciclos e imprimir el resultado
+"""
+animales = {"Pinguino", "Flamenco", "Panda", "Leon", "Serpiente"}
+otros_animales = {"Gato", "Perros", "Panda rojo", "Leon", "Flamenco"}
+print(animales.intersection(otros_animales))
