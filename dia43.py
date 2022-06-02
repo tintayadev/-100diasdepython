24ed3f60c1ffeea2319fd00c1d5bffe71895c8bd
tintayadev <pau.tintaya.code@gmail.com>
2022-06-01 21:15:48 -0400

    Day 43

---
+++ dia43.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 43 de #100diasdepython
+            El reto de hoy es:
+Declara una tupla de 5 simbolos que representen magnitudes
+fisicas e imprime el resultado
+"""
+magnitudes_fisicas = ("Hz", "N", "m", "s", "ºC")
+print(magnitudes_fisicas)
