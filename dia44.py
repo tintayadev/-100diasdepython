6c3ea284f5e7a2139c7879576596a74983e93a83
tintayadev <pau.tintaya.code@gmail.com>
2022-06-02 14:16:50 -0400

    Day 44

---
+++ dia44.py
@@ -0,0 +1,7 @@
+"""
+    Biienvenidos al dia 44 de #100diasdepython
+            El reto de hoy es:
+Usa la tupla del ejemplo anterior e imprime los primeros 3 elementos
+"""
+magnitudes_fisicas = ("Hz", "N", "m", "s", "ºC")
+print(magnitudes_fisicas[:3])
