39d131b246b07058f5eb722981bb4f7b882e40c3
tintayadev <pau.tintaya.code@gmail.com>
2022-06-03 20:16:14 -0400

    Day 45

---
+++ dia45.py
@@ -0,0 +1,9 @@
+"""
+    Biienvenidos al dia 45 de #100diasdepython
+            El reto de hoy es:
+Usa la tupla del ejemplo anterior, elimina los
+primeros 2 elementos e imprime el resultado
+"""
+magnitudes_fisicas = ("Hz", "N", "m", "s", "ºC")
+magnitudes_fisicas = magnitudes_fisicas[2:]
+print(magnitudes_fisicas)
