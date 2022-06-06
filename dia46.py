21b0820a65f64a11a0b3218697717a760e636361
tintayadev <pau.tintaya.code@gmail.com>
2022-06-06 14:48:20 -0400

    Day 46

---
+++ dia46.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 46 de #100diasdepython
+            El reto de hoy es:
+Crea una lista de 10 numeros usando la funcion range()
+e imprime el resultado
+"""
+numbers = list(range(10))
+print(numbers)
