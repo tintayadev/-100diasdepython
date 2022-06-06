8646648016d7085f2891adf4566a9a2dbdf9788e
tintayadev <pau.tintaya.code@gmail.com>
2022-06-06 14:48:38 -0400

    Day 47

---
+++ dia47.py
@@ -0,0 +1,8 @@
+"""
+    Biienvenidos al dia 47 de #100diasdepython
+            El reto de hoy es:
+Crea una lista de 5 numeros pares usando 
+la funcion range() e imprime el resultado
+"""
+numbers = list(range(0, 10, 2))
+print(numbers)
