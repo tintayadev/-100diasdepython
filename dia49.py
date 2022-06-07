e99636f76e11cddbe9d43aef5ff4c25dba90b1a4
tintayadev <pau.tintaya.code@gmail.com>
2022-06-07 19:16:39 -0400

    Day 49

---
+++ dia49.py
@@ -0,0 +1,11 @@
+"""
+    Bienvenidos al dia 49 de #100diasdepython
+            El reto de hoy es:
+Declara una variable numerica con valor 100
+Decrementa su valor de 10 en 10 mientras no sea 0
+Imprime el valor de la variable en cada iteracion
+"""
+number = 100
+while(number != 0):
+    print(number)
+    number -= 10
