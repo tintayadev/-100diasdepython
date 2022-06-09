06830247478895cd8d2c21207ae75ce94e0bb394
tintayadev <pau.tintaya.code@gmail.com>
2022-06-09 11:31:52 -0400

    Day 51

---
+++ dia51.py
@@ -0,0 +1,14 @@
+"""
+    Bienvenidos al dia 50 de #100diasdepython
+            El reto de hoy es:
+Crea una funcion que calule el volumen de un cilindro, los parametros
+de entrada son base y altura, y el volumen de salida es el volumen del cilindro
+Ejecuta la funcion para el caso base = 5, altura=7
+Imprime el resultado
+"""
+
+def calculaVolumenCilindro(base, altura):
+    return base*altura
+
+volumen_cilindro = calculaVolumenCilindro(5, 7)
+print(volumen_cilindro)
