abf7354c19cfea3f4d34ebf530bfbbf3aeab13a5
tintayadev <pau.tintaya.code@gmail.com>
2022-07-12 19:06:18 -0400

    Day 80

---
+++ dia80.py
@@ -0,0 +1,11 @@
+"""
+    Bienvenidos al dia 79 de #100diasdepython
+            El reto de hoy es:
+Utiliza itertools para repetir el numero 80 
+5 veces en una lista
+Imprime el resultado en una lista
+"""
+import itertools
+lista = list(itertools.repeat(80,5)) 
+    
+print(lista)
