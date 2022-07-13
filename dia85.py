29834f94a139f2d132e1e5ca2acd470c19ecd0de
tintayadev <pau.tintaya.code@gmail.com>
2022-07-13 16:47:06 -0400

    Day 85

---
+++ dia85.py
@@ -0,0 +1,9 @@
+"""
+    Bienvenidos al dia 85 de #100diasdepython
+            El reto de hoy es:
+Utiliza el datetime para imprimir la fecha y hora actual
+en UTC
+"""
+from datetime import datetime
+print(datetime.utcnow())
+
