9fb49fbd7e7e1513213165f69001093776265578
tintayadev <pau.tintaya.code@gmail.com>
2022-07-15 18:46:13 -0400

    Day 87

---
+++ dia87.py
@@ -0,0 +1,10 @@
+"""
+    Bienvenidos al dia 87 de #100diasdepython
+            El reto de hoy es:
+Utiliza calendar para imprimir el mes de Julio 
+en formato calendario
+"""
+import calendar
+
+print(calendar.month(2022, 7))
+
